a264097ba06714e7f2f4cc5fff2a070ac1969b1b
huajiang <1739756046@qq.com>
2026-06-30 20:44:12 +0800

    add 增加other-agent,以便于开发a2aagent

---
+++ other-agents/src/main/java/com/h/otheragents/OtherAgentsApplication.java
@@ -0,0 +1,13 @@
+package com.h.otheragents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtherAgentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtherAgentsApplication.class, args);
+    }
+
+}
---
+++ other-agents/src/test/java/com/h/otheragents/OtherAgentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h.otheragents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtherAgentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
